8ce733f6721fb37dc2b627cdf1657cf3f9c1c8b4
roxyrypler <30288569+roxyrypler@users.noreply.github.com>
2018-12-08 16:09:42 +0100

    Getting all character from all books
    There is a around 4935 api requests, it takes around 3.08s to load. Adding placeholders and intro so that it can load in the background.

--- Public/js/lobby.js
+++ Public/js/lobby.js
@@ -1,5 +1,45 @@
 
 
 
+/* --------------------------------------------------- */
+allBooks();
 
+//got main data
+function allBooks() {
+	for (let i = 1; i < 12; i++) {
+		getData(i)
+			.then(data => gotData(data))
+			.catch(reason => console.log(reason.message));	
+	}
+}
 
+async function getData(i) {
+	let apiAdress = "https://anapioficeandfire.com/api/books/" + i;
+	let response = await fetch(apiAdress);
+	let data = await response.json();
+	return data;
+}
+
+function gotData(data) {
+	//console.log(data);
+	for (let i = 0; i < data.characters.length; i++) {
+		//get char names and info
+		getCharCal(data, i)
+			.then(data => gotCharData(data))
+			.catch(reason => console.log(reason.message));
+	}
+}
+
+async function getCharCal(chardata, i) {
+		let response = await fetch(chardata.characters[i]);
+		let data = await response.json();
+		return data;
+}
+
+function gotCharData(data) {
+	//console.log(data.name);
+	if (data.name == "Daenerys Targaryen") {
+		console.log(data.name);
+	}
+}
+/* --------------------------------------------------- */
