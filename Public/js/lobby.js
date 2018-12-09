b5b08586ee6a3248a62c9cf416553bad2430cebe
roxyrypler <30288569+roxyrypler@users.noreply.github.com>
2018-12-09 17:24:48 +0100

    Hell of a lot of unnecessary api calls here, but i have to do those i guess.

--- Public/js/lobby.js
+++ Public/js/lobby.js
@@ -1,5 +1,17 @@
-
-
+/* --------------------------------------------------- */
+//Global vars
+let callCount = 0;
+let namesArray = [];
+let charnames = ["Tyrion Lannister",
+				"Daenerys Targaryen",
+				"Cersei Lannister",
+				"Jon Snow",
+				"Sansa Stark",
+				"Arya Stark",//?missing from api
+				"Eddard Stark",
+				"Jaime Lannister",
+				"Tywin Lannister",
+				"Joffrey Baratheon"];
 
 /* --------------------------------------------------- */
 allBooks();
@@ -9,7 +21,7 @@ function allBooks() {
 	for (let i = 1; i < 12; i++) {
 		getData(i)
 			.then(data => gotData(data))
-			.catch(reason => console.log(reason.message));	
+			.catch(reason => console.log(reason.message));
 	}
 }
 
@@ -28,6 +40,7 @@ function gotData(data) {
 			.then(data => gotCharData(data))
 			.catch(reason => console.log(reason.message));
 	}
+	
 }
 
 async function getCharCal(chardata, i) {
@@ -37,12 +50,68 @@ async function getCharCal(chardata, i) {
 }
 
 function gotCharData(data) {
-	//console.log(data.name);
-	if (data.name == "Daenerys Targaryen") {
-		console.log(data.name);
+	//Add unique names to array
+	if (namesArray.includes(data.name) == false) {
+		namesArray.push({name: data.name,
+						 born: data.born,
+						 aliases: data.aliases[0]});
+	}
+	callCount++;
+	if (callCount == 4921) {
+		settingNames();
 	}
 }
 /* --------------------------------------------------- */
 
 //adding 10 chars for the player
+function settingNames() {
+	for (let j = 1; j < 11; j++) {
+		for (let i = 0; i < namesArray.length; i++) {
+			//console.log(namesArray[i].name);
+			if (namesArray[i].name == charnames[j-1]) {
+				let p1Charname = document.getElementById("p1Char" + j + "Name").innerHTML = namesArray[i].name;
+				let p1CharAlias = document.getElementById("p1Char" + j + "Alias").innerHTML = namesArray[i].aliases;
+				let p1CharBorn = document.getElementById("p1Char" + j + "Born").innerHTML = "Born " +  namesArray[i].born;
+
+				let p2Charname = document.getElementById("p2Char" + j + "Name").innerHTML = namesArray[i].name;
+				let p2CharAlias = document.getElementById("p2Char" + j + "Alias").innerHTML = namesArray[i].aliases;
+				let p2CharBorn = document.getElementById("p2Char" + j + "Born").innerHTML = "Born " +  namesArray[i].born;
+			}
+		}	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
